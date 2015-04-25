06f45f085f1aa8b77bbbb1222e1d7f8a0e969b3d
Roman Bekkiev <rbekkiev@Romans-Mac-mini.local>
2015-04-25 20:40:38 +0300

    working tests for server-storage

--- src/js/lib/__tests__/server-storage.test.js
+++ src/js/lib/__tests__/server-storage.test.js
@@ -1,8 +1,213 @@
-jest.dontMock('../server-storage');
+jest.dontMock('lib/server-storage');
 jest.dontMock('global/window');
+jest.dontMock('es6-promise');
 
-describe('sum', function() {
-	it('', function() {
+const serverStorage = require('lib/server-storage');
+const Promise = require('es6-promise').Promise; // jshint ignore:line
+const xhr = require('xhr');
+const HOST = require('constants/app-constants').API_HOST;
+const HTTPError = require('errors/http-error');
 
+function createAsyncSpies(){
+	var spies = {
+		flag: false,
+		thenSpy: jasmine
+			.createSpy('thenSpy')
+			.andCallFake(() => {
+				spies.flag = true;
+			}),
+		catchSpy: jasmine
+			.createSpy('catchSpy')
+			.andCallFake(() => {
+				spies.flag = true;
+			})
+	};
+	return spies;
+}
+
+function createResponce(status, message){ // mninimal responce object based on XHR API description - https://www.npmjs.com/package/xhr
+	return {statusCode: status, rawRequest:{statusText: message}, headers: {}};
+}
+
+afterEach(function() {
+	xhr.mockClear();
+});
+
+describe('serverStorage.get', function() {
+
+	it('should return a Promice', function() {
+		expect(serverStorage.get()).toEqual(jasmine.any(Promise));
 	});
+
+	it('should call xhr with proper URL', function(){
+		serverStorage.get('/foo');
+		expect(xhr).toBeCalledWith({
+			uri: HOST + '/foo'
+		}, jasmine.any(Function));
+	});
+
+});
+
+describe('handling the response (by the example of GET request)', function(){
+
+	it('should reject promice if error in browser', function(){
+		var spies = createAsyncSpies();
+
+		runs(function(){
+			serverStorage.get('/foo').then(spies.thenSpy).catch(spies.catchSpy);
+			xhr.mock.calls[0][1]('some error'); // xhr calls callback with some error in fitst argument
+		});
+
+		waitsFor(function(){return spies.flag;}, 'Promise should be resolved or rejected', 10);
+
+		runs(function(){
+			expect(spies.thenSpy).not.toHaveBeenCalled();
+			expect(spies.catchSpy).toHaveBeenCalled();
+		});
+	});
+
+	it('should reject promice with HTTPError with status code 0 if error in browser', function(){
+		var spies = createAsyncSpies();
+
+		runs(function(){
+			serverStorage.get('/foo').then(spies.thenSpy).catch(spies.catchSpy);
+			xhr.mock.calls[0][1]('some error');
+		});
+
+		waitsFor(function(){return spies.flag;}, 'Promise should be resolved or rejected', 10);
+
+		runs(function(){
+			var error = spies.catchSpy.mostRecentCall.args[0];
+			expect(error).toEqual(jasmine.any(HTTPError));
+			expect(error.status).toEqual(0);
+			expect(error.message).toEqual('Request declined by browser');
+		});
+	});
+
+	it('should reject promice if responce status code is 404', function(){
+		var spies = createAsyncSpies();
+
+		runs(function(){
+			serverStorage.get('/foo').then(spies.thenSpy).catch(spies.catchSpy);
+			xhr.mock.calls[0][1](null, createResponce(404));
+		});
+
+		waitsFor(function(){return spies.flag;}, 'Promise should be resolved or rejected', 10);
+
+		runs(function(){
+			expect(spies.thenSpy).not.toHaveBeenCalled();
+			expect(spies.catchSpy).toHaveBeenCalled();
+		});
+	});
+
+	it('should reject promice if responce status code is 500', function(){
+		var spies = createAsyncSpies();
+
+		runs(function(){
+			serverStorage.get('/foo').then(spies.thenSpy).catch(spies.catchSpy);
+			xhr.mock.calls[0][1](null, createResponce(500));
+		});
+
+		waitsFor(function(){return spies.flag;}, 'Promise should be resolved or rejected', 10);
+
+		runs(function(){
+			expect(spies.thenSpy).not.toHaveBeenCalled();
+			expect(spies.catchSpy).toHaveBeenCalled();
+		});
+	});
+
+	it('should reject promice with HTTPError with proper status code and message', function(){
+		var spies = createAsyncSpies();
+
+		runs(function(){
+			serverStorage.get('/foo').then(spies.thenSpy).catch(spies.catchSpy);
+			xhr.mock.calls[0][1](null, createResponce(401, 'Unauthorized'));
+		});
+
+		waitsFor(function(){return spies.flag;}, 'Promise should be resolved or rejected', 10);
+
+		runs(function(){
+			var error = spies.catchSpy.mostRecentCall.args[0];
+			expect(error).toEqual(jasmine.any(HTTPError));
+			expect(error.status).toEqual(401);
+			expect(error.message).toEqual('Unauthorized');
+		});
+	});
+
+	it('should resolove promise if status code is 200', function(){
+		var spies = createAsyncSpies();
+
+		runs(function(){
+			serverStorage.get('/foo').then(spies.thenSpy).catch(spies.catchSpy);
+			xhr.mock.calls[0][1](null, createResponce(200));
+		});
+
+		waitsFor(function(){return spies.flag;}, 'Promise should be resolved or rejected', 10);
+
+		runs(function(){
+			expect(spies.thenSpy).toHaveBeenCalled();
+			expect(spies.catchSpy).not.toHaveBeenCalled();
+		});
+	});
+
+	it('should provide responce body if request fullfiled', function(){
+		var spies = createAsyncSpies();
+		var body = 'foo bar';
+
+		runs(function(){
+			serverStorage.get('/foo').then(spies.thenSpy).catch(spies.catchSpy);
+			var resp = createResponce(200);
+			resp.body = body;
+			xhr.mock.calls[0][1](null, resp);
+		});
+
+		waitsFor(function(){return spies.flag;}, 'Promise should be resolved or rejected', 10);
+
+		runs(function(){
+			var resp = spies.thenSpy.mostRecentCall.args[0];
+			expect(resp).toEqual(body);
+		});
+	});
+
+	it('shoudl parse JSON if content-type is JSON', function(){
+		var spies = createAsyncSpies();
+		var body = {foo: 'bar'};
+
+		runs(function(){
+			serverStorage.get('/foo').then(spies.thenSpy).catch(spies.catchSpy);
+			var resp = createResponce(200);
+			resp.body = JSON.stringify(body);
+			resp.headers['content-type'] = 'application/json';
+			xhr.mock.calls[0][1](null, resp);
+		});
+
+		waitsFor(function(){return spies.flag;}, 'Promise should be resolved or rejected', 10);
+
+		runs(function(){
+			var resp = spies.thenSpy.mostRecentCall.args[0];
+			expect(resp).toEqual(body);
+		});
+	});
+
+	it('should reject promice with TypeError if content-type is JSON but responce is not JSON (and message should say something about JSON)', function(){
+		var spies = createAsyncSpies();
+		var body = 'foo bar';
+
+		runs(function(){
+			serverStorage.get('/foo').then(spies.thenSpy).catch(spies.catchSpy);
+			var resp = createResponce(200);
+			resp.body = body;
+			resp.headers['content-type'] = 'application/json';
+			xhr.mock.calls[0][1](null, resp);
+		});
+
+		waitsFor(function(){return spies.flag;}, 'Promise should be resolved or rejected', 10);
+
+		runs(function(){
+			var error = spies.catchSpy.mostRecentCall.args[0];
+			expect(error).toEqual(jasmine.any(TypeError));
+			expect(error).toMatch(/JSON/i);
+		});
+	});
+
 });
--- src/js/lib/server-storage.js
+++ src/js/lib/server-storage.js
@@ -1,15 +1,20 @@
 const xhr = require('xhr');
 const HOST = require('constants/app-constants').API_HOST;
 const HTTPError = require('errors/http-error');
+const Promise = require('es6-promise').Promise; // jshint ignore:line
 
 function handleResponce(err, res, resolve, reject){
 
-	if(err) reject(new HTTPError(0, 'Request declined by browser'));
+	if(err) return reject(new HTTPError(0, 'Request declined by browser'));
 
-	if(res.statusCode >= 400) reject(new HTTPError(res.statusCode, res.rawRequest.statusText));
+	if(res.statusCode >= 400) return reject(new HTTPError(res.statusCode, res.rawRequest.statusText));
 
 	if(res.headers['content-type'] === 'application/json'){
-		resolve(JSON.parse(res.body));
+		try{
+			resolve(JSON.parse(res.body));
+		} catch (e) {
+			reject(new TypeError('Content-Type of the server response specified as JSON but response is not a JSON'));
+		}
 	} else {
 		resolve(res.body);
 	}
