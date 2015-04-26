ec80a27844d67e6455d4ce740178ec52aa274dc2
Roman Bekkiev <rbekkiev@Romans-Mac-mini.local>
2015-04-26 23:15:12 +0300

    icons for files

--- src/js/components/file/component.jsx
+++ src/js/components/file/component.jsx
@@ -1,26 +1,42 @@
 const React = require('react');
 const moment = require('moment');
+const {FontIcon} = require('material-ui');
 
+const mdiClasses = {
+	folder: 'mdi-file-folder',
+	document: 'mdi-action-description',
+	file: 'mdi-editor-insert-drive-file',
+	casefile: 'mdi-communication-quick-contacts-mail',
+	unknown: 'mdi-communication-live-help'
+};
+function getIconName(type){
+	return (mdiClasses[type] || mdiClasses.unknown) + ' file__icon';
+}
 
 let App = React.createClass({
+
+
 
 	render() {
 		return (
 			<li className="file">
-				<span className="file__data-item">
-					{this.props.data.title}
-				</span>
-				<span className="file__data-item">
-					{this.props.data.status}
-				</span>
-				<span className="file__data-item">
-					{this.props.data.type}
-				</span>
-				<span className="file__data-item">
-					{moment(this.props.data.created).fromNow()}
-				</span>
-				<span className="file__data-item">
-					{moment(this.props.data.modified).fromNow()}
+				<FontIcon className={getIconName(this.props.data.type)}/>
+				<span className="file__data">
+					<span className="file__data-item">
+						{this.props.data.title}
+					</span>
+					<span className="file__data-item">
+						{this.props.data.status}
+					</span>
+					<span className="file__data-item">
+						{this.props.data.type}
+					</span>
+					<span className="file__data-item">
+						{moment(this.props.data.created).fromNow()}
+					</span>
+					<span className="file__data-item">
+						{moment(this.props.data.modified).fromNow()}
+					</span>
 				</span>
 			</li>
 		);
