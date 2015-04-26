fb2855d5edc82fc5b4c13684ae3fb517a1e32095
bertolomych <rom.bekk@gmail.com>
2015-04-26 12:22:26 +0300

    file model-1

---
+++ src/js/models/file.js
@@ -0,0 +1,14 @@
+function File(data){
+	this.id = data.id;
+	this.title = data.title;
+	this.status = data.status;
+	this.type = data.type;
+	this.created = data.created * 1000;
+	this.modified = data.modified * 1000;
+}
+
+Object.defineProperty(File, 'FIELDS', {
+	value: ['title','status','type','created','modified']
+});
+
+module.exports = File;
